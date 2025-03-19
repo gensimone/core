568f47e76ce732a19fbd6423f92cff6f14f1a5d9
Simone Gentili <gensimone.git@gmail.com>
2025-03-19 13:43:20 +0100

    update-grub removed. using grub-mkconfig

--- .core/core.d/grub.sh
+++ .core/core.d/grub.sh
@@ -16,11 +16,4 @@ GRUB_DISTRIBUTOR="Void"
 GRUB_CMDLINE_LINUX_DEFAULT="loglevel=3"
 EOF
 
-if which update-grub; then
-  sudo update-grub
-elif which grub-mkconfig; then
-  sudo grub-mkconfig -o /boot/grub/grub.cfg
-else
-  echo "Missing update-grub/grub-mkconfig in PATH" >&2
-  exit 1
-fi
+sudo grub-mkconfig -o /boot/grub/grub.cfg
