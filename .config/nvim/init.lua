c443faa943354936307a5a37b96dff4b04e50d66
Simone Gentili <gensimone.git@gmail.com>
2026-01-24 12:25:27 +0100

    Update nvim

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,3 +1,97 @@
-require("config.lazy")
-require("config.options")
-require("config.keymap")
+-- Bootstrap lazy.nvim
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+  if vim.v.shell_error ~= 0 then
+    vim.api.nvim_echo({
+      { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+      { out, "WarningMsg" },
+      { "\nPress any key to exit..." },
+    }, true, {})
+    vim.fn.getchar()
+    os.exit(1)
+  end
+end
+vim.opt.rtp:prepend(lazypath)
+
+-- Make sure to setup `mapleader` and `maplocalleader` before
+-- loading lazy.nvim so that mappings are correct.
+-- This is also a good place to setup other settings (vim.opt)
+vim.g.mapleader = " "
+vim.g.maplocalleader = "\\"
+
+local opt = vim.opt
+local cmd = vim.cmd
+local diagnostic = vim.diagnostic.config
+opt.cursorline = false
+opt.expandtab = true
+opt.hlsearch = false
+opt.ignorecase = true
+opt.incsearch = true
+opt.number = true
+opt.relativenumber = true
+opt.scrolloff = 8
+opt.shiftwidth = 4
+opt.signcolumn = "no"
+opt.smartcase = true
+opt.smartindent = true
+opt.tabstop = 4
+opt.termguicolors = true
+opt.undofile = true
+opt.updatetime = 300
+opt.wrap = false
+cmd("set shm+=I")
+cmd("set noshowmode")
+cmd("set noshowcmd")
+cmd("colorscheme lunaperche")
+cmd [[autocmd BufWritePre * lua vim.lsp.buf.format()]]
+diagnostic({ underline = false })
+
+-- Rounded corners
+vim.o.winborder = "bold"
+
+-- Automatically remove trailing whitespace
+vim.api.nvim_create_autocmd("BufWritePre", {
+  pattern = "*",
+  command = [[%s/\s\+$//e]],
+})
+
+-- Setup lazy.nvim
+require("lazy").setup({
+  spec = {
+    -- import your plugins
+    { import = "plugins" },
+  },
+  -- automatically check for plugin updates
+  checker = { enabled = false },
+  change_detection = { enabled = false }
+})
+
+local opts = { noremap = true, silent = true }
+local keymap = vim.keymap.set
+local telescope_builtin = require('telescope.builtin')
+keymap('n', '<leader>ff', telescope_builtin.find_files)
+keymap('n', '<leader>fr', telescope_builtin.oldfiles)
+keymap('n', '<leader>fg', telescope_builtin.live_grep)
+keymap('n', '<leader>fb', telescope_builtin.buffers)
+keymap('n', '<leader>fh', telescope_builtin.help_tags)
+keymap('n', '<leader>fm', function() telescope_builtin.man_pages({ sections = { 'ALL' } }) end)
+keymap('n', '<leader>fc', function() telescope_builtin.find_files({ cwd = vim.fn.stdpath('config') }) end)
+keymap('n', '<leader>fp', ':NeovimProjectDiscover<CR>')
+keymap('n', '<leader>ds', telescope_builtin.diagnostics)
+keymap('n', '<leader>bd', ':bd<CR>')
+keymap('n', '<leader>n', ':bn<CR>')
+keymap('n', '<leader>p', ':bp<CR>')
+keymap('n', '<leader>w', ':w<CR>')
+keymap('n', '<leader>q', ':q<CR>')
+keymap('n', '<leader>e', ':Oil<CR>')
+keymap('n', '<leader>g', ':Neogit<CR>')
+keymap('n', '<leader>r', ':lua vim.lsp.buf.rename()<CR>')
+keymap('n', '<leader>y', '"+y')
+keymap('t', '<C-h>', [[<C-\><C-n><C-w>h]], opts)
+keymap('t', '<C-j>', [[<C-\><C-n><C-w>j]], opts)
+keymap('t', '<C-k>', [[<C-\><C-n><C-w>k]], opts)
+keymap('t', '<C-l>', [[<C-\><C-n><C-w>l]], opts)
+keymap({'n', 'x'}, "gz", "<Cmd>MultipleCursorsAddMatches<CR>")
+keymap({'n', 'x'}, "<C-n>", "<Cmd>MultipleCursorsAddJumpNextMatch<CR>")
