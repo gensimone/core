f42e7c471e206de8374853d7634d625d06c567cc
Simone Gentili <gensimone.git@gmail.com>
2026-02-15 12:55:29 +0100

    Update suckless.sh

--- .arch/arch.d/suckless.sh
+++ .arch/arch.d/suckless.sh
@@ -7,10 +7,24 @@ check_dep() {
     fi
 }
 
+check_dep gcc
 check_dep git
+check_dep libxft
+check_dep libxinerama
 check_dep make
 check_dep pkg-config
 
 DWM="https://github.com/gensimone/dwm"
 ST="https://github.com/gensimone/st"
 DMENU="https://github.com/gensimone/dmenu"
+
+install() {
+    if ! git clone $1 $2 && make -C $2 && rm -rfv $2; then
+        echo "Aborted." >&2
+        exit 1
+    fi
+}
+
+install dwm
+install st
+install dmenu
