d21686cf3bd12493e5e239ae1e5751616548d904
Simone Gentili <gensimone.git@gmail.com>
2026-02-15 13:25:37 +0100

    Update suckless.sh

--- .arch/arch.d/suckless.sh
+++ .arch/arch.d/suckless.sh
@@ -1,19 +1,5 @@
 #!/bin/sh
 
-check_dep() {
-    if ! command -v $1 >/dev/null 2>&1; then
-        printf "%s could not be found\n" $1 >&2
-        exit 1
-    fi
-}
-
-check_dep gcc
-check_dep git
-check_dep libxft
-check_dep libxinerama
-check_dep make
-check_dep pkg-config
-
 DWM="https://github.com/gensimone/dwm"
 ST="https://github.com/gensimone/st"
 DMENU="https://github.com/gensimone/dmenu"
