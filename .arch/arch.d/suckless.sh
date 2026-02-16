04c2d3122868dd8f7b19cf67d22f58199cdc7ce9
Simone Gentili <gensimone.git@gmail.com>
2026-02-16 15:48:50 +0100

    Update .arch

--- .arch/arch.d/suckless.sh
+++ .arch/arch.d/suckless.sh
@@ -1,20 +1,14 @@
 #!/bin/sh
 
-DWM="https://github.com/gensimone/dwm"
-ST="https://github.com/gensimone/st"
-DMENU="https://github.com/gensimone/dmenu"
-SLSTATUS="https://github.com/gensimone/slstatus"
-
 install() {
-    git clone --depth=1 $1 $2 && sudo make clean install -C $2 && rm -rfv $2
+    name="$(basename $1)"
+    git clone --depth=1 $1 $name && sudo make clean install -C $name && rm -rfv $name
     [ $? -gt 0 ] && echo "Aborted." >&2 && exit 1
 }
 
-#       repo      pkg name
-install $DWM      dwm
-install $ST       st
-install $DMENU    dmenu
-install $SLSTATUS slstatus
+install "https://github.com/gensimone/dwm"
+install "https://github.com/gensimone/st"
+install "https://github.com/gensimone/dmenu"
 
 sudo mkdir -vp /usr/share/xsessions
 cat <<EOF | sudo tee /usr/share/xsessions/dwm.desktop 1>/dev/null
